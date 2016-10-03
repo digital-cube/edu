bfaa7ecf6aabd29eb76164f6c6f9ceb571e3ebc2
Igor Jeremic <igor@digitalcube.rs>
2016-10-03 10:58:09 +0200

    tornado

--- python/tornado/2018-09-30/server.py
+++ python/tornado/2018-09-30/server.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
+import tornado.template
 import tornado.web
 import socket
+import json
 import os
 
 
@@ -9,24 +11,42 @@ class IndexHandler(tornado.web.RequestHandler):
 
         self.render("html/index.html",
                     title="home page",
-                    header=self.render_string('html/tpl/header.html'),
-                    footer='sloba',
-                    content='mladen<b>xx</b>'
                     )
+
+
+class NewsHandler(tornado.web.RequestHandler):
+    def get(self):
+
+        news = json.load(open('news/index.json'))
+        for item in news:
+            if 'html-content' in item:
+                with open('news/'+item['html-content'], 'r') as f:
+                    item['content'] = f.read()
+
+        self.render("html/news.html",
+                    title="news page",
+                    news=news
+                    )
+
+
+class NewsModule(tornado.web.UIModule):
+    def render(self, news):
+        return self.render_string('modules/news.html', news=news)
 
 
 def make_app():
 
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "css"),
+        "ui_modules": {"News": NewsModule},
     }
 
     return tornado.web.Application([
-        (r"/", IndexHandler),
-        (r'/css/(main\.css)',
-            tornado.web.StaticFileHandler,
-            {'path': settings['static_path']}),
-    ], debug = socket.gethostname() in ['Igors-Air',])
+                (r"/", IndexHandler),
+                (r"/news", NewsHandler),
+                (r'/css/(main\.css)', tornado.web.StaticFileHandler, {'path': settings['static_path']}),
+                ],
+            debug=socket.gethostname() in ['Igors-Air',], **settings)
 
 
 if __name__ == "__main__":
