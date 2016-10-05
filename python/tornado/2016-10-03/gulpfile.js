c79bae7c8bbee8e06f435c7e8abc3657ba491a11
Mladen <mladen@digitalcube.rs>
2016-10-05 19:31:18 +0200

    mladen changes

--- python/tornado/2016-10-03/gulpfile.js
+++ python/tornado/2016-10-03/gulpfile.js
@@ -2,15 +2,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src(['css/*.scss', 'css/pages/*.scss', 'css/inc/*.scss'])
+    return gulp.src(['scss/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch(['css/*.scss'], ['sass']);
-    gulp.watch(['css/pages/*.scss'], ['sass']);
-    gulp.watch(['css/inc/*.scss'], ['sass']);
+    gulp.watch(['scss/*.scss'], ['sass']);
 });
 
 gulp.task('default', ['sass', 'watch'], function() {});
