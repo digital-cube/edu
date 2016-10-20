fcbc07cbe923a078d03d42884fe54cd07ea171c8
Andrija <andrija@digitalcube.rs>
2016-10-20 09:24:52 +0200

    Ispravljeno da radi i u opsegu od 0 do 1

--- projecteuler/andrija/koren.java
+++ projecteuler/andrija/koren.java
@@ -6,16 +6,31 @@ public class koren {
 	
 	
 	static public double sqrt(double broj, double p){
+		double b=0;
 		double min = 0;
 		double max = broj;
-		double b=0;
-		while(max-min>p){
-			b = (max+min)/2;
-			if(b*b>broj) {
-				max = b;
-			} else {
-				min = b;
+		if(broj>1){
+			while(max-min>p){
+				b = (max+min)/2;
+				if(b*b>broj) {
+					max = b;
+				} else {
+					min = b;
+				}
 			}
+		} else if(broj<1){
+			min = broj;
+			max = 1;
+			while(max-min>p){
+				b = (max+min)/2;
+				if(b*b>broj) {
+					max = b;
+				} else {
+					min = b;
+				}
+			}
+		} else {
+			b=1;
 		}
 
 		return b;
@@ -24,6 +39,8 @@ public class koren {
 	public static void main(String[] args){
 		
 		System.out.println("koren je: " + sqrt(25, 0.001));
+		System.out.println("koren je: " + sqrt(0.81, 0.001));
+		System.out.println("koren je: " + sqrt(1, 0.001));
 		
 	}
 
